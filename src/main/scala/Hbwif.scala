9b0bf5ab36bca8c34a87294e606b84ddf2ec79ad
Colin Schmidt <colins@eecs.berkeley.edu>
2016-09-27 14:28:32 -0700

    fixups for rocketchip integration

--- src/main/scala/Hbwif.scala
+++ src/main/scala/Hbwif.scala
@@ -15,10 +15,8 @@ case class HbwifParameters(
   val numLanes: Int = 8,
   val bufferDepth: Int = 10) // Calculated based on worst-case transmission line delay and codec, serdes, etc. latency
 
-trait HasHbwifParameters extends HasBertParameters with HasTransceiverParameters {
+trait HasHbwifParameters extends HasBertParameters with HasTransceiverParameters with HasPeripheryParameters {
   val hbwifNumLanes = p(HbwifKey).numLanes
-  val outermostParams = p.alterPartial({ case TLId => "Outermost" })
-  val outermostMMIOParams = p.alterPartial({ case TLId => "MMIO_Outermost" })
 }
 
 trait Hbwif extends LazyModule
@@ -27,33 +25,32 @@ trait Hbwif extends LazyModule
 
   (0 until hbwifNumLanes).foreach { i =>
     // TODO: 1024 is arbitrary, calculate this number from the SCR size
-    pDevices.add(AddrMapEntry(s"hbwif_lane$i", MemSize(1024, MemAttr(AddrMapProt.RW))))
+    pDevices.add(AddrMapEntry(s"hbwif_lane$i", MemSize(4096, MemAttr(AddrMapProt.RW))))
   }
-  pDevices.add(AddrMapEntry("hbwif_", MemSize(1024, MemAttr(AddrMapProt.RW))))
 }
 
 trait HbwifBundle extends HasHbwifParameters {
   val hbwifRx      = Vec(hbwifNumLanes, new Differential)
   val hbwifTx      = Vec(hbwifNumLanes, (new Differential)).flip
   val hbwifIref    = if(transceiverRefGenHasInput) Some(Bool(INPUT)) else None
-  val hbwifFastClk = Bool(INPUT)
 }
 
 trait HbwifModule extends HasHbwifParameters {
   implicit val p: Parameters
   val io: HbwifBundle
-  val mmioNetwork: Option[TileLinkRecursiveInterconnect]
+  val pBus: TileLinkRecursiveInterconnect
   val hbwifIO: Vec[ClientUncachedTileLinkIO]
+  val hbwifFastClock: Clock
 
   val hbwifLanes = Seq.fill(hbwifNumLanes) { Module(new Lane) }
 
-  hbwifLanes.foreach { _.io.fastClk := io.hbwifFastClk }
+  hbwifLanes.foreach { _.io.fastClk := hbwifFastClock }
 
-  hbwifLanes.map(_.io.rx).zip(io.hbwifRx) map { case (lane, top) => lane <> top }
+  hbwifLanes.map(_.io.rx).zip(io.hbwifRx) map { case (lane, top) => top <> lane }
   hbwifLanes.map(_.io.tx).zip(io.hbwifTx) map { case (lane, top) => lane <> top }
 
   (0 until hbwifNumLanes).foreach { i =>
-    hbwifLanes(i).io.scr <> mmioNetwork.get.port(s"hbwif_lane$i")
+    hbwifLanes(i).io.scr <> pBus.port(s"hbwif_lane$i")
   }
   hbwifLanes.zip(hbwifIO).foreach { x => x._1.io.mem <> x._2 }
 
