d96d89e8a2743a2cdeb2aa8ae0a4ea171d6d16a4
John Wright <johnwright@eecs.berkeley.edu>
2018-04-24 17:29:39 -0700

    remove dlev

--- src/main/resources/generic_transceiver.sv
+++ src/main/resources/generic_transceiver.sv
@@ -10,7 +10,6 @@
 module generic_transceiver (
   input [`SERDES_BITS-1:0] data_tx,
   output [`SERDES_BITS-1:0] data_rx,
-  output [`SERDES_BITS-1:0] data_dlev,
   output tx_p,
   output tx_n,
   input rx_p,
