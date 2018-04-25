3e2b37bd8d3522e456f6560eb196fd5c3b73554a
Albert Ou <aou@eecs.berkeley.edu>
2018-04-24 17:36:40 -0700

    Define WithGenericSerdes mixin

--- src/main/scala/tilelink/Config.scala
+++ src/main/scala/tilelink/Config.scala
@@ -3,6 +3,7 @@ package hbwif.tilelink
 import hbwif._
 import freechips.rocketchip.config._
 import freechips.rocketchip.diplomacy.AddressSet
+import freechips.rocketchip.subsystem.{ExtMem, BankedL2Key, MemoryBusKey}
 
 case class HbwifTLConfig(
     managerAddressSet: AddressSet,
@@ -26,4 +27,37 @@ case object HbwifSerDesKey extends Field[SerDesConfig]
 case object HbwifBertKey extends Field[BertConfig]
 case object HbwifTLKey extends Field[HbwifTLConfig]
 case object HbwifPatternMemKey extends Field[PatternMemConfig]
+case object BuildHbwif extends Field[Parameters => HbwifModule]
+
+class WithGenericSerdes extends Config((site, here, up) => {
+    case HbwifSerDesKey => SerDesConfig(
+        dataWidth = 16,
+        numWays = 2
+    )
+    case HbwifTLKey => {
+        val nMemoryChannels = site(BankedL2Key).nMemoryChannels
+        HbwifTLConfig(
+            managerAddressSet = AddressSet(site(ExtMem).get.base, site(ExtMem).get.size - 1),
+            configAddressSets = Seq.tabulate(nMemoryChannels) { i =>
+                AddressSet(0x4000000 + i*0x10000, 0xffff)
+            },
+            numLanes = nMemoryChannels,
+            numBanks = 2,
+            beatBytes = site(MemoryBusKey).beatBytes,
+            numXact = 16,
+            tluh = true,
+            tlc = true,
+            asyncQueueDepth = 8,
+            asyncQueueSync = 3,
+            asyncQueueSafe = true,
+            asyncQueueNarrow = true
+        )
+    }
+    case HbwifBertKey => BertConfig.fromBER(site(HbwifSerDesKey), 1e-15)
+    case HbwifPatternMemKey => PatternMemConfig(
+        patternDepth = 16,
+        snapshotDepth = 16
+    )
+    case BuildHbwif => (p: Parameters) => new GenericHbwifModule()(p)
+})
 
