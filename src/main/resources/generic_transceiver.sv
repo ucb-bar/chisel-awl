77ea3e380c71134713ce761cd4ae512051b0c70e
John Wright <johnwright@eecs.berkeley.edu>
2018-04-27 12:57:46 -0700

    fix verilog simulation bug

--- src/main/resources/generic_transceiver.sv
+++ src/main/resources/generic_transceiver.sv
@@ -39,13 +39,9 @@ module generic_transceiver (
   assign clock_rx_div = clock_tx_div_reg;
 
   always @(posedge clock_fast or negedge clock_fast) begin
-    count <= (count + 1) % `SERDES_BITS;
-  end
-
-  always @(posedge clock_fast or negedge clock_fast) begin
-    #0.01
-    if (count == `SERDES_BITS/2-1) clock_tx_div_reg <= 1'b0;
-    else if (count == `SERDES_BITS-1) clock_tx_div_reg <= 1'b1;
+    if (count == `SERDES_BITS/2-1) clock_tx_div_reg = 1'b0;
+    else if (count == `SERDES_BITS-1) clock_tx_div_reg = 1'b1;
+    count = (count + 1) % `SERDES_BITS;
   end
 
   //***********************************
